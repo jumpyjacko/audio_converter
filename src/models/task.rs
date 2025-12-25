4fd6a1842cacdff9491d29c567eb297fe447cbef
Jackson Ly <bobjacklee27@gmail.com>
2025-12-25 11:42:35 +1100

    add: message passing for task status

--- src/models/task.rs
+++ src/models/task.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 
 use crate::app;
@@ -6,41 +7,49 @@ use crate::transcode;
 
 #[derive(Debug, Clone)]
 enum TaskStatus {
-    NotStarted,
     Started,
     Paused,
     Failed,
     Completed,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Task {
     file: AudioFile,
-    progress: u8,
-    status: TaskStatus,
+    status: Option<mpsc::Receiver<TaskStatus>>,
 }
 
 impl Task {
     pub fn new(file: AudioFile) -> Self {
         return Task {
             file,
-            progress: 0,
-            status: TaskStatus::NotStarted,
+            status: None,
         };
     }
 
     pub fn start_transcode(&mut self, settings: &app::Settings) {
         let file = self.file.clone();
         let settings = settings.clone();
+        let (tx, rx) = mpsc::channel();
+
+        self.status = Some(rx);
+        let _ = tx.send(TaskStatus::Started);
 
         thread::spawn(move || {
-            let _ = transcode::convert_file(
+            match transcode::convert_file(
                 file,
                 &settings.out_codec,
                 settings.out_bitrate,
                 &settings.out_directory,
                 &settings.out_container,
-            );
+            ) {
+                Ok(_) => {
+                    let _ = tx.send(TaskStatus::Completed);
+                },
+                Err(_) => {
+                    let _ = tx.send(TaskStatus::Failed);
+                },
+            }
         });
     }
 }
